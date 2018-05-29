0f65097ff535a508beba230b3855f4329de72e2a
Aaron Humerickhouse <aaron.humerickhouse@bigcommerce.com>
2018-05-29 15:34:11 -0500

    Working with like, term, eachLike

--- test/pact/idea/get_idea.test.pact.js
+++ test/pact/idea/get_idea.test.pact.js
@@ -1,10 +1,29 @@
 const Pact = require('@pact-foundation/pact');
 const axios = require('axios');  // This is your client-side API layer
+const term = Pact.Matchers.term
+const like = Pact.Matchers.somethingLike
+const eachLike = Pact.Matchers.eachLike
 
 describe('Idea API', () => {
   let url = 'http://localhost:3001';
 
-  const EXPECTED_GET_IDEA_BODY = {"id":1, "title":"A new cake recipe", "body":"Made of chocolate", "created_at":"2018-05-24T22:41:38.190Z", "updated_at":"2018-05-24T22:41:38.190Z"}
+  const DATE_MATCHER = '\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z'
+
+  const INTERACTION_GET_IDEA_BODY = {
+    "id": like(1),
+    "title":like("A new cake recipe"),
+    "body":like("Made of chocolate"),
+    "created_at": term({matcher: DATE_MATCHER, generate: "2018-05-24T22:41:38.190Z"}),
+    "updated_at":term({matcher: DATE_MATCHER, generate: "2018-05-24T22:41:38.190Z"})
+  }
+
+  const EXPECTED_GET_IDEA_BODY = {
+    "id":1,
+    "title":"A new cake recipe",
+    "body":"Made of chocolate",
+    "created_at":"2018-05-24T22:41:38.190Z",
+    "updated_at":"2018-05-24T22:41:38.190Z"
+  }
 
   // Copy this block once per interaction under test
   describe('GET :id'/* The API interaction being tested in words (string) */, () => {
@@ -17,17 +36,18 @@ describe('Idea API', () => {
           path:  '/api/v1/ideas/1.json'/* '/foo/bar' (string) */,
           query:  ''/* '?query=parameters' (string) */,
           headers: {
-            Accept: 'application/json'
+            Accept: 'application/json',
+            charset: 'utf-8'
             /* etc */
           }
         },
         willRespondWith: {
           status: 200,
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json; charset=utf-8'
             /* etc */
           },
-          body: EXPECTED_GET_IDEA_BODY/* describe the body (object using the Pact DSL)*/
+          body: INTERACTION_GET_IDEA_BODY/* describe the body (object using the Pact DSL)*/
         }
       };
       return provider.addInteraction(interaction);
@@ -35,9 +55,9 @@ describe('Idea API', () => {
 
     // add expectations
     it('returns single idea object with matching id'/* describe the test */, done => {
-      axios.get(url + '/api/v1/ideas/1.json', { headers: {"Accept": "application/json"}})
+      axios.get(url + '/api/v1/ideas/1.json', { headers: {"Accept": "application/json", "charset": "utf-8"}})
         .then(response => {
-          expect(response.headers['content-type']).toEqual('application/json')
+          expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
           expect(response.data).toEqual(EXPECTED_GET_IDEA_BODY)
           expect(response.status).toEqual(200)
           done()
--- test/pact/idea/get_ideas.test.pact.js
+++ test/pact/idea/get_ideas.test.pact.js
@@ -1,15 +1,29 @@
 const Pact = require('@pact-foundation/pact');
 const axios = require('axios');  // This is your client-side API layer
+const term = Pact.Matchers.term
+const like = Pact.Matchers.somethingLike
+const eachLike = Pact.Matchers.eachLike
 
 describe('Idea API', () => {
   let url = 'http://localhost:3001';
 
-  const EXPECTED_GET_IDEAS_BODY = [
-    {"id":4, "title":"Card game design", "body":"Like Uno but involves drinking", "created_at":"2018-05-24T22:41:38.195Z", "updated_at":"2018-05-24T22:41:38.195Z"},
-    {"id":3, "title":"A novel set in Italy", "body":"A mafia crime drama starring Berlusconi", "created_at":"2018-05-24T22:41:38.194Z", "updated_at":"2018-05-24T22:41:38.194Z"},
-    {"id":2, "title":"A twitter client idea", "body":"Only for replying to mentions and DMs", "created_at":"2018-05-24T22:41:38.192Z", "updated_at":"2018-05-24T22:41:38.192Z"},
-    {"id":1, "title":"A new cake recipe", "body":"Made of chocolate", "created_at":"2018-05-24T22:41:38.190Z", "updated_at":"2018-05-24T22:41:38.190Z"}
-  ]
+  const DATE_MATCHER = '\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z'
+  const INTERACTION_GET_IDEA_BODY = {
+    "id": like(1),
+    "title":like("A new cake recipe"),
+    "body":like("Made of chocolate"),
+    "created_at": term({matcher: DATE_MATCHER, generate: "2018-05-24T22:41:38.190Z"}),
+    "updated_at":term({matcher: DATE_MATCHER, generate: "2018-05-24T22:41:38.190Z"})
+  }
+
+  const INTERACTION_GET_IDEAS_BODY = eachLike(INTERACTION_GET_IDEA_BODY)
+
+  // const EXPECTED_GET_IDEAS_BODY = [
+  //   {"id":4, "title":"Card game design", "body":"Like Uno but involves drinking", "created_at":"2018-05-24T22:41:38.195Z", "updated_at":"2018-05-24T22:41:38.195Z"},
+  //   {"id":3, "title":"A novel set in Italy", "body":"A mafia crime drama starring Berlusconi", "created_at":"2018-05-24T22:41:38.194Z", "updated_at":"2018-05-24T22:41:38.194Z"},
+  //   {"id":2, "title":"A twitter client idea", "body":"Only for replying to mentions and DMs", "created_at":"2018-05-24T22:41:38.192Z", "updated_at":"2018-05-24T22:41:38.192Z"},
+  //   {"id":1, "title":"A new cake recipe", "body":"Made of chocolate", "created_at":"2018-05-24T22:41:38.190Z", "updated_at":"2018-05-24T22:41:38.190Z"}
+  // ]
 
   // Copy this block once per interaction under test
   describe('GET'/* The API interaction being tested in words (string) */, () => {
@@ -22,17 +36,17 @@ describe('Idea API', () => {
           path:  '/api/v1/ideas.json'/* '/foo/bar' (string) */,
           query:  ''/* '?query=parameters' (string) */,
           headers: {
-            Accept: 'application/json'
-            /* etc */
+            Accept: 'application/json',
+            charset: 'utf-8'
           }
         },
         willRespondWith: {
           status: 200,
           headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json; charset=utf-8'
             /* etc */
           },
-          body: EXPECTED_GET_IDEAS_BODY/* describe the body (object using the Pact DSL)*/
+          body: INTERACTION_GET_IDEAS_BODY/* describe the body (object using the Pact DSL)*/
         }
       };
       return provider.addInteraction(interaction);
@@ -40,10 +54,10 @@ describe('Idea API', () => {
 
     // add expectations
     it('returns an array of idea objects'/* describe the test */, done => {
-      axios.get(url + '/api/v1/ideas.json', { headers: {"Accept": "application/json"}})
+      axios.get(url + '/api/v1/ideas.json',  { headers: {"Accept": "application/json", "charset": "utf-8"}})
         .then(response => {
-          expect(response.headers['content-type']).toEqual('application/json')
-          expect(response.data).toEqual(EXPECTED_GET_IDEAS_BODY)
+          expect(response.headers['content-type']).toEqual('application/json; charset=utf-8')
+          // expect(response.data).toEqual(EXPECTED_GET_IDEAS_BODY)
           expect(response.status).toEqual(200)
           done()
           /* check the response here, using the default values provided to the Pact DSL */
