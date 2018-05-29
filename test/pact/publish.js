f5ceb70dd3ab6cd18f7673c521cd6ecb389bf754
Aaron Humerickhouse <aaron.humerickhouse@bigcommerce.com>
2018-05-29 12:44:05 -0500

    Working publishing

--- test/pact/publish.js
+++ test/pact/publish.js
@@ -4,7 +4,7 @@ let path = require('path');
 let opts = {
   providerBaseUrl: 'http://localhost:3001',
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pacts')],
-  pactBroker: 'https://localhost:9292',
+  pactBroker: 'http://localhost:9292',
   pactBrokerUsername: '',//process.env.PACT_USERNAME,
   pactBrokerPassword: '',//process.env.PACT_PASSWORD,
   consumerVersion: '1.0.0'
